c1ebe9d7db1e4d0a3a8b18c434088e16107d1438
Jordan Faust <jordan.faust@cerner.com>
2016-09-16 17:22:18 -0500

    Switch to linum + custom modification to get linenum highlighting

--- spacemacs/private/spacemacs-doom/packages.el
+++ spacemacs/private/spacemacs-doom/packages.el
@@ -58,12 +58,14 @@
     highlight-parentheses
     info+
     iedit
+    web-mode
     ;; neotree
 
     ;; Additional packages from Doom
     beacon
     hl-line
-    nlinum
+    ;; (nlinum :location local)
+    linum
     ))
 
 (defun spacemacs-doom/init-eval-sexp-fu ()
@@ -790,99 +792,10 @@ It will toggle the overlay under point or create an overlay of one character."
                    'face 'font-lock-warning-face))
             (force-mode-line-update)))))))
 
-;; (defun spacemacs-doom/init-neotree ()
-;;   (use-package neotree
-;;     :defer t
-;;     :commands neo-global--window-exists-p
-;;     :init
-;;     (progn
-;;       (setq neo-window-width 32
-;;             neo-create-file-auto-open t
-;;             neo-banner-message nil
-;;             neo-show-updir-line nil
-;;             neo-mode-line-type 'neotree
-;;             neo-smart-open t
-;;             neo-dont-be-alone t
-;;             neo-persist-show nil
-;;             neo-show-hidden-files t
-;;             neo-auto-indent-point t
-;;             neo-modern-sidebar t
-;;             neo-vc-integration nil)
-
-;;       (defun spacemacs/neotree-expand-or-open ()
-;;         "Collapse a neotree node."
-;;         (interactive)
-;;         (let ((node (neo-buffer--get-filename-current-line)))
-;;           (when node
-;;             (if (file-directory-p node)
-;;                 (progn
-;;                   (neo-buffer--set-expand node t)
-;;                   (neo-buffer--refresh t)
-;;                   (when neo-auto-indent-point
-;;                     (next-line)
-;;                     (neo-point-auto-indent)))
-;;               (call-interactively 'neotree-enter)))))
-
-;;       (defun spacemacs/neotree-collapse ()
-;;         "Collapse a neotree node."
-;;         (interactive)
-;;         (let ((node (neo-buffer--get-filename-current-line)))
-;;           (when node
-;;             (when (file-directory-p node)
-;;               (neo-buffer--set-expand node nil)
-;;               (neo-buffer--refresh t))
-;;             (when neo-auto-indent-point
-;;               (neo-point-auto-indent)))))
-
-;;       (defun spacemacs/neotree-collapse-or-up ()
-;;         "Collapse an expanded directory node or go to the parent node."
-;;         (interactive)
-;;         (let ((node (neo-buffer--get-filename-current-line)))
-;;           (when node
-;;             (if (file-directory-p node)
-;;                 (if (neo-buffer--expanded-node-p node)
-;;                     (spacemacs/neotree-collapse)
-;;                   (neotree-select-up-node))
-;;               (neotree-select-up-node)))))
-
-;;       (defun neotree-find-project-root ()
-;;         (interactive)
-;;         (if (neo-global--window-exists-p)
-;;             (neotree-hide)
-;;           (let ((origin-buffer-file-name (buffer-file-name)))
-;;             (neotree-find (projectile-project-root))
-;;             (neotree-find origin-buffer-file-name))))
-
-;;       (defun spacemacs//neotree-key-bindings ()
-;;         "Set the key bindings for a neotree buffer."
-;;         (evilified-state-evilify-map neotree-mode-map
-;;           :mode neotree-mode
-;;           :bindings
-;;           (kbd "TAB")  'neotree-stretch-toggle
-;;           (kbd "RET") 'neotree-enter
-;;           (kbd "|") 'neotree-enter-vertical-split
-;;           (kbd "-") 'neotree-enter-horizontal-split
-;;           (kbd "?") 'evil-search-backward
-;;           (kbd "c") 'neotree-create-node
-;;           (kbd "d") 'neotree-delete-node
-;;           (kbd "gr") 'neotree-refresh
-;;           (kbd "h") 'spacemacs/neotree-collapse-or-up
-;;           (kbd "H") 'neotree-select-previous-sibling-node
-;;           (kbd "J") 'neotree-select-down-node
-;;           (kbd "K") 'neotree-select-up-node
-;;           (kbd "l") 'spacemacs/neotree-expand-or-open
-;;           (kbd "L") 'neotree-select-next-sibling-node
-;;           (kbd "q") 'neotree-hide
-;;           (kbd "r") 'neotree-rename-node
-;;           (kbd "R") 'neotree-change-root
-;;           (kbd "s") 'neotree-hidden-file-toggle))
-
-;;       (spacemacs/set-leader-keys
-;;         "ft" 'neotree-toggle
-;;         "pt" 'neotree-find-project-root))
-
-;;     :config
-;;     (spacemacs//neotree-key-bindings)))
+(defun spacemacs-doom/init-web-mode ()
+  (use-package web-mode
+    :config
+    (add-to-list 'auto-mode-alist '("\\.xml.erb\\'" . web-mode))))
 
 (defun spacemacs-doom/init-beacon ()
   (use-package beacon
@@ -908,28 +821,37 @@ It will toggle the overlay under point or create an overlay of one character."
     (add-hook 'evil-visual-state-entry-hook 'doom|hl-line-off)
     (add-hook 'evil-visual-state-exit-hook  'doom|hl-line-on)))
 
-(defun spacemacs-doom/init-nlinum ()
-  (use-package nlinum
-    :commands nlinum-mode
+(defun spacemacs-doom/init-linum ()
+  (use-package linum
     :preface
-    (setq linum-format "%3d ")
-    (defvar nlinum-format "%4d ")
-    (defvar doom--hl-nlinum-overlay nil)
-    (defvar doom--hl-nlinum-line nil)
-    :init
-    (add-hook!
-      (markdown-mode prog-mode scss-mode web-mode conf-mode groovy-mode
-                     nxml-mode snippet-mode php-mode)
-      'nlinum-mode)
-    ;; FIXME This only works if hl-line is active!
-    (add-hook! nlinum-mode
-      (if nlinum-mode-hook
-          (add-hook 'post-command-hook 'doom|nlinum-hl-line nil t)
-        (remove-hook 'post-command-hook 'doom|nlinum-hl-line t)))
+    (defvar linum-current-line 1 "Current line number.")
+    (defvar linum-border-width 4 "Border width for linum.")
+
+    (defface linum-current-line
+      `((t :inherit doom-linum
+           :background "#262c34"
+          ))
+      "Face for displaying the current line number."
+      :group 'linum)
+
+    (defadvice linum-update (before advice-linum-update activate)
+      "Set the current line."
+      (setq linum-current-line (line-number-at-pos)
+            ;; It's the same algorithm that linum dynamic. I only had added one
+            ;; space in front of the first digit.
+            linum-border-width (number-to-string
+                                (length
+                                 (number-to-string
+                                  (count-lines (point-min) (point-max)))))))
+
+    (defun linum-highlight-current-line (line-number)
+      "Highlight the current line number using `linum-current-line' face."
+      (let ((face (if (= line-number linum-current-line)
+                      'doom-nlinum-highlight
+                    'linum)))
+        (propertize (format (concat "%" linum-border-width "d ") line-number)
+                    'face face)))
     :config
-    ;; Calculate line number column width
-    (add-hook! nlinum-mode
-      (setq nlinum--width (length (save-excursion (goto-char (point-max))
-                                                  (format-mode-line "%l")))))))
+    (setq linum-format 'linum-highlight-current-line)))
 
 ;;; packages.el ends here
